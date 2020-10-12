bce5dddb68a0ea271cf7bc4d4d2d15f1eb814a80
longbot <baolongred2k@gmail.com>
2020-10-12 12:57:50 +0700

    data

---
+++ data.js
@@ -0,0 +1,132 @@
+const product=[
+    {
+        category : `apparel`,
+                // + shoes 
+                // + accessories
+        name : `WE ARE PINK MEDIUM FIT HOODIE`,
+        brand : `balenciaga`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black','while'],
+        gender : ['male'],
+                // female
+        price : 950,
+        productType : `hoodie`,
+    },
+    {
+        category : `apparel`,
+                // + shoes 
+                // + accessories
+        name : `WE ARE PINK LARGE FIT T-SHIRT`,
+        brand : `balenciaga`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black','while'],
+        gender : ['male'],
+                // female
+        price : 510,
+        productType : `t-shirt`,
+    },
+    {
+        category : `accessories`,
+                // + shoes 
+                // + apparel
+        name : `NEO CLASSIC LARGE TOP HANDLE BAG`,
+        brand : `balenciaga`,
+        size : 'no size',
+        color : ['black','while'],
+        gender : ['female'],
+                
+        price : 2650,
+        productType : `bag`,
+    },
+    {
+        category : `accessories`,
+                // + shoes 
+                // + apparel
+        name : `NEO CLASSIC SMALL TOP HANDLE BAG`,
+        brand : `balenciaga`,
+        size : 'no size',
+        color : ['black','grey'],
+        gender : ['female'],
+                
+        price : 2250,
+        productType : `bag`,
+    },
+    {
+        category : `accessories`,
+                // + shoes 
+                // + apparel
+        name : `NEO CLASSIC MINI TOP HANDLE BAG`,
+        brand : `balenciaga`,
+        size : 'no size',
+        color : ['black','blue','red'],
+        gender : ['female'],
+                
+        price : 1980,
+        productType : `bag`,
+    },
+    {
+        category : `shoe`,
+                // + accessories 
+                // + apparel
+        name : `TRACK SLIDE SANDAL`,
+        brand : `balenciaga`,
+        size : [4,4.5,5,5.5,6,6.5,7,7.5,8],
+        color : ['black','while'],
+        gender : ['female'],
+                //male
+        price : 630,
+        productType : `track`,
+    },
+    {
+        category : `shoe`,
+                // + accessories 
+                // + apparel
+        name : `TRACK SNEAKER`,
+        brand : `balenciaga`,
+        size : [4,4.5,5,5.5,6,6.5,7,7.5,8,8,5,9],
+        color : ['black','while','blue',],
+        gender : ['female'],
+                //male
+        price : 1005,
+        productType : `track`,
+    },
+    {
+        category : `shoe`,
+                // + accessories 
+                // + apparel
+        name : `TRIPLE S SNEAKER`,
+        brand : `balenciaga`,
+        size : [4,4.5,5,5.5,6,6.5,7,7.5,8],
+        color : ['black','while','red','blue','grey'],
+        gender : ['female'],
+                //male
+        price : 1110,
+        productType : `triple s`,
+    },
+    {
+        category : `shoe`,
+                // + accessories 
+                // + apparel
+        name : `SPEED SNEAKER`,
+        brand : `balenciaga`,
+        size : [4,4.5,5,5.5,6,6.5,7,7.5,8],
+        color : ['black','while','muticolor'],
+        gender : ['female'],
+                //male
+        price : 840,
+        productType : `speed`,
+    },
+    {
+        category : `shoe`,
+                // + accessories 
+                // + apparel
+        name : `SPEED 2.0 SNEAKER`,
+        brand : `balenciaga`,
+        size : [4,4.5,5,5.5,6,6.5,7,7.5,8],
+        color : ['black','while','red','blue','grey','muticolor'],
+        gender : ['female'],
+                //male
+        price : 950,
+        productType : `speed 2.0`,
+    },
+]
